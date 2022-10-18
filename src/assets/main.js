1c496cfa110323e806600bb2b2a4f66c6cbabfa3
Enrique Vizarra <vizarramattay@gmail.com>
2022-10-18 10:30:40 -0500

    FEAT: function anonima

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,7 @@
 
-const API = "https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=RDZiQo7nAkQHU&part=snippet&maxResults=50"
+const API = "https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=RDZiQo7nAkQHU&part=snippet&maxResults=50";
+
+const content = null || document.getElementById("content");
 
 const options = {
 	method: 'GET',
@@ -14,4 +16,28 @@ async function fetchData(urlApi){
   const data = await response.json();
   return data
 }
+(async ()=> {
+  try{
+    const videos = await fetchData(API);
+    const view = `
+      ${videos.items.map(video => `
+        <div class="group relative">
+          <div
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          </div>
+          <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+              <span aria-hidden="true" class="absolute inset-0"></span>
+              ${video.snippet.title}
+            </h3>
+          </div>
+        </div>
+      `).slice(0,4).join("")}
+    `;
+    content.innerHTML = view; 
+  }catch(error){
+    console.log(error)
+  }
+})();
 
